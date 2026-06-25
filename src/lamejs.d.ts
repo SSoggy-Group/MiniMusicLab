8995932501040e01cebf746ce00cbaa116a51c31
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-06-25 19:39:43 +0200

    feat: add lamejs typescript module declarations

---
+++ src/lamejs.d.ts
@@ -0,0 +1,8 @@
+declare namespace lamejs {
+  export class Mp3Encoder {
+    constructor(channels: number, samplerate: number, kbps: number)
+    encodeBuffer(left: Int16Array, right?: Int16Array): Int8Array
+    flush(): Int8Array
+  }
+}
+
